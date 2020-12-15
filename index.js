386e51835297f9089f7c1b417d1585483910ce4f
Никита Батрак <batnik63@gmail.com>
2020-12-15 22:18:09 +0300

    коммит с подключением апи к проекту(старт положен)

---
+++ index.js
@@ -0,0 +1,9 @@
+$.ajax({
+    type:"GET",
+    url:"http://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&APPID=e9d5091db0a0df91f5931bb9abb550a2",
+    success:
+            function (data) {
+            console.log(data);
+        
+    }
+})
