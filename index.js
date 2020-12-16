b5ecad522826105bba36940da5ac3fe1cb02a874
Никита Батрак <batnik63@gmail.com>
2020-12-16 21:15:19 +0300

    new branch

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ $.ajax({
     url:"http://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&APPID=e9d5091db0a0df91f5931bb9abb550a2",
     success:
             function (data) {
-            weatherObject(data);
-                  
+            weatherObject(data);                
     }
 })
 
