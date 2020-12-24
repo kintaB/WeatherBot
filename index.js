57a200682cf3c5fc914a876b01faacf8cd9f9ec1
Никита Батрак <batnik63@gmail.com>
2020-12-25 02:03:08 +0300

    Короче это ласт коммит но если чо надо я добавлю

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 process.env.NTBA_FIX_319 = 1;
+let cheerio = require("cheerio");
 const request = require('request-promise');
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 var config = require("config").config;
 const token = process.env.BT_TOKEN;
 const appId = process.env.BT_APPID;
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, {
+  polling: true
+});
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const options = {
-    method:"GET",
-    uri:"http://api.openweathermap.org/data/2.5/weather?id=524901&appid="+ appId + "&lang=ru&q=" + msg.text
-};
+    method: "GET",
+    uri: "http://api.openweathermap.org/data/2.5/weather?id=524901&appid=" + appId + "&lang=ru&q=" + msg.text
+  };
   options.uri = encodeURI(options.uri);
-  
-  request(options)
-  .then(function(res){
-    let weather = JSON.parse(res),
-    temp = weather.main.temp - 250,
-    feels_like = weather.main.feels_like,
-    pressure = weather.main.pressure,
-    message = "ВАШ ГОРОД - "+ weather.name + "\n\nВНИМАНИЕ ТЕМПЕРАТУРА ОТОБРАЖАЕТСЯ В КЕЛЬВИНАХ А ДАВЛЕНИЕ ХУЙ ПОЙМИ В ЧЕМ ВРОДЕ В ММ РТ СТ!!!!!!\n\n"+"Температура будет " + temp + "\n НО Ощущается как "+ feels_like + "\nНе знаю зачем тебе оно, старик,но давление будет "+ pressure;
-    bot.sendMessage(chatId,message);
-  })  
-  .catch(function (err) {
-    console.log(err)
-    bot.sendMessage(chatId,'Харош писать что попало')
-})
+  processing(chatId, options)
 });
+
+function processing(chatId, options) {
+  request(options)
+    .then(function (res) {
+      let weather = JSON.parse(res),
+        temp = weather.main.temp - 273.15,
+        feels_like = weather.main.feels_like - 273.15,
+        pressure = weather.weather[0].description,
+        message = "ВАШ ГОРОД - " + weather.name + 
+        "\n\n Не все могут смотреть в завтрашний день!!!!!!\n\n" +
+        "Температура будет " + temp.toFixed(1) +
+        "\nНО Ощущается как " + feels_like.toFixed(1) +
+        "\nНе знаю зачем тебе оно, но на улице " + pressure;
+        
+      bot.sendMessage(chatId, message);
+    })
+    .catch(function (err) {
+      console.log(err)
+      bot.sendMessage(chatId, 'Харош писать что попало')
+    })
+}
