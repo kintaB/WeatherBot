91c9b1d5db40b6ec26bebd57c079f090683b0d3f
Никита Батрак <batnik63@gmail.com>
2020-12-25 01:20:55 +0300

    Добавление конфига, небольшое изменение кода

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 process.env.NTBA_FIX_319 = 1;
 const request = require('request-promise');
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1405219646:AAE4kjGSn-dVnIM6Br1H3cdShrLE5EvjYWg';
+require('dotenv').config();
+var config = require("config").config;
+const token = process.env.BT_TOKEN;
+const appId = process.env.BT_APPID;
 const bot = new TelegramBot(token, {polling: true});
-
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const options = {
     method:"GET",
-    uri:'http://api.openweathermap.org/data/2.5/weather?q=' + msg.text +  '&APPID=e9d5091db0a0df91f5931bb9abb550a2&lang=ru',
+    uri:"http://api.openweathermap.org/data/2.5/weather?id=524901&appid="+ appId + "&lang=ru&q=" + msg.text
 };
+  options.uri = encodeURI(options.uri);
+  
   request(options)
   .then(function(res){
-    bot.sendMessage(chatId,res)
+    let weather = JSON.parse(res),
+    temp = weather.main.temp - 250,
+    feels_like = weather.main.feels_like,
+    pressure = weather.main.pressure,
+    message = "ВАШ ГОРОД - "+ weather.name + "\n\nВНИМАНИЕ ТЕМПЕРАТУРА ОТОБРАЖАЕТСЯ В КЕЛЬВИНАХ А ДАВЛЕНИЕ ХУЙ ПОЙМИ В ЧЕМ ВРОДЕ В ММ РТ СТ!!!!!!\n\n"+"Температура будет " + temp + "\n НО Ощущается как "+ feels_like + "\nНе знаю зачем тебе оно, старик,но давление будет "+ pressure;
+    bot.sendMessage(chatId,message);
   })  
   .catch(function (err) {
     console.log(err)
-    bot.sendMessage(chatId,'что-то не так')
+    bot.sendMessage(chatId,'Харош писать что попало')
 })
 });
